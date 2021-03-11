8c59758d8dc2f2cf8521b61ffb6ea5f728f3be1c
ehuang93 <64566074+ehuang93@users.noreply.github.com>
2021-03-10 16:36:10 -0800

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ from sklearn.linear_model import LinearRegression
 
 model = pkl.load(open('deployment.pkl','rb'))
 
+input_dict = {'Avg. Session Length':30.00000, 'Time on App':12.00000, 'Time on Website':37.00000, 'Length of Membership':3.00000}
+input_df = pd.DataFrame([input_dict])
+
 def test_predict():
-    assert 0 < predict(model=model, input_df={'Avg. Session Length':30.00000, 'Time on App':12.00000, 'Time on Website':37.00000, 'Length of Membership':3.0000})
+    assert 0 < predict(model=model, input_df=input_df)
