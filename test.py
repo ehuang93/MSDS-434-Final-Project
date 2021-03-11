2e4bf18ea2f6375451800eb1500cf2c5e4dcacff
ehuang93 <64566074+ehuang93@users.noreply.github.com>
2021-03-10 16:29:43 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from main import predict
+import pickle as pkl
+from sklearn.linear_model
+
+model = pkl.load(open('deployment.pkl','rb'))
 
 def test_predict():
     assert 0 < predict(model=model, input_df={'Avg. Session Length':30.00000, 'Time on App':12.00000, 'Time on Website':37.00000, 'Length of Membership':3.0000})
